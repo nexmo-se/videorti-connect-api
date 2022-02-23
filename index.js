be62e6943fb6269f29001e3dda634eab5f84d518
javiermolsanz <javier.molinasanz@vonage.com>
2022-02-23 15:06:54 +0000

    adding code

---
+++ index.js
@@ -0,0 +1,176 @@
+'use strict';
+require('dotenv').config();
+const StreamingClient = require('./streamingClient.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const expressWs = require('express-ws')(app);
+const axios = require('axios');
+const { Readable } = require('stream');
+const jwt = require('jsonwebtoken');
+const speech = require('@google-cloud/speech');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+  );
+  next();
+});
+
+app.post('/startStreaming', async (req, res) => {
+  try {
+    console.log('someone wants to stream');
+    const streamId = req.body.streamId;
+    // console.log(streamId);
+    const response = await startStreamer(streamId);
+    res.send(response);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.ws('/socket', async (ws, req) => {
+  console.log('someone connected');
+  const uuid = req?.query?.uuid ? req.query.uuid : 'dummyuuid';
+  // let uuid = 'dummyuuid';
+  let sc = new StreamingClient(uuid, 'en-US', 'es');
+  await sc.init();
+  const aWss = expressWs.getWss().clients;
+
+  // aWss.forEach(function (client) {
+  //   console.log(client);
+  // });
+
+  // Array.from(expressWs.getWss().clients).map((sock) => {
+  //   console.log(sock.route);
+  // }); /* <- Your path */
+  // }).forEach(function (client) {
+  //   client.send(msg.data);
+  // });
+
+  // setTimeout(() => {
+  //   ws.send('{"original":"mis huevos"}');
+  // }, 3000);
+
+  sc.setAudioChunkAvailableCallback(function (chunk) {
+    console.log('audio available');
+    const aWss = expressWs.getWss('/socket').clients;
+    if (ws.readyState === 1) {
+      aWss.forEach(function (client) {
+        // console.log(client);
+
+        client.send(chunk);
+      });
+      // ws.send(chunk);
+    }
+  });
+
+  sc.setTranscriptionAvailableCallback(function (data) {
+    console.log(data);
+    const aWss = expressWs.getWss('/socket').clients;
+    aWss.forEach(function (client) {
+      // console.log(client);
+      // console.log(client._socket.server);
+      client.send(JSON.stringify(data));
+    });
+    // ws.send(JSON.stringify(data));
+  });
+  sc.startRecognizer();
+  ws.on('message', (msg) => {
+    try {
+      if (typeof msg === 'string') {
+        let config = JSON.parse(msg);
+        console.log(config);
+        sc.setId(config.from);
+      } else {
+        // console.log(msg);
+        sc.sendMessage(msg);
+      }
+    } catch (err) {
+      console.log('[' + uuid + '] ' + err);
+      ws.removeAllListeners('message');
+      ws.close();
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('[' + uuid + '] Websocket closed');
+    sc.closeConversation();
+    sc = null;
+  });
+});
+
+const generateRestToken = () => {
+  return new Promise((res, rej) => {
+    jwt.sign(
+      {
+        iss: process.env.apiKey,
+        // iat: Date.now(),
+        ist: 'project',
+        exp: Date.now() + 200,
+        jti: Math.random() * 132,
+      },
+      process.env.apiSecret,
+      { algorithm: 'HS256' },
+      function (err, token) {
+        if (token) {
+          console.log('\n Received token\n', token);
+          res(token);
+        } else {
+          console.log('\n Unable to fetch token, error:', err);
+          rej(err);
+        }
+      }
+    );
+  });
+};
+
+const startStreamer = async (streamId) => {
+  try {
+    // const { sessionId, token, apiKey } = await getCredentials();
+
+    const data = JSON.stringify({
+      sessionId: process.env.sessionId,
+      token: process.env.token,
+      websocket: {
+        uri: 'ws://31c9-2-220-24-117.ngrok.io/socket',
+        streams: [streamId],
+        headers: {
+          from: streamId,
+        },
+      },
+    });
+
+    const config = {
+      method: 'post',
+      url: `https://api.dev.opentok.com/v2/project/${process.env.apiKey}/connect`,
+      headers: {
+        'X-OPENTOK-AUTH': await generateRestToken(),
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+    // console.log(config);
+    const response = await axios(config);
+    console.log(response.data);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+  console.log(`Server application listening on port ${port}!`)
+);
---
+++ streamingClient.js
@@ -0,0 +1,163 @@
+'use strict';
+require('dotenv').config();
+const GOOGLE_APPLICATION_CREDENTIALS =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const model = 'command_and_search';
+const speech = require('@google-cloud/speech');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const { Translate } = require('@google-cloud/translate').v2;
+const fs = require('fs');
+const util = require('util');
+
+class StreamingClient {
+  constructor(id, language, outputLanguage) {
+    this.id = id;
+    this.init = this.init.bind(this);
+    this.recognizeStream = null;
+    this.ttsBuffer = null;
+    this.ttsBufferSize = 0;
+    this.currentTTSChunk = 0;
+    this.ttsTimer = undefined;
+    this.language = language;
+    this.outputLanguage = outputLanguage;
+    this.waitingRestart = true;
+    this.sessionEnding = false;
+    this.audioChunkAvailableCallback = undefined;
+    this.transcriptAvailableCallback = undefined;
+    this.streamingLimit = 290000;
+    this.intervalRestart = setInterval(() => {
+      this.tryEndAudioStream();
+      this.startRecognizer();
+    }, this.streamingLimit);
+    this.errorCallback = undefined;
+  }
+
+  setAudioChunkAvailableCallback(callback) {
+    this.audioChunkAvailableCallback = callback;
+  }
+  setTranscriptionAvailableCallback(callback) {
+    this.transcriptAvailableCallback = callback;
+  }
+  setId(id) {
+    this.id = id;
+  }
+
+  async init() {
+    this.speechClient = new speech.SpeechClient();
+    this.ttsClient = new textToSpeech.TextToSpeechClient();
+    this.translationClient = new Translate();
+  }
+
+  sendMessage(msg) {
+    if (!this.waitingRestart) {
+      this.recognizeStream.write(msg);
+      // fs.appendFileSync('out.pcm',msg);
+    }
+  }
+
+  clearInterval() {
+    clearInterval(this.intervalRestart);
+  }
+
+  startRecognizer() {
+    const config = {
+      encoding: encoding,
+      sampleRateHertz: sampleRateHertz,
+      model: model,
+      enableAutomaticPunctuation: true,
+      languageCode: this.language,
+    };
+
+    const request = {
+      config: config,
+      interimResults: false,
+    };
+
+    this.recognizeStream = this.speechClient.streamingRecognize(request);
+    this.recognizeStream.on('data', async (data) => {
+      let originalText = data.results[0].alternatives[0].transcript;
+      console.log('[' + this.id + '][User]' + originalText);
+      let translatedText = await this.translate(originalText);
+      if (this.transcriptAvailableCallback) {
+        this.transcriptAvailableCallback({
+          original: originalText,
+          translated: translatedText,
+          id: this.id,
+        });
+      }
+      this.playTTS(translatedText);
+    });
+
+    this.recognizeStream.on('error', (err) => {
+      console.error(err);
+      this.tryEndAudioStream();
+      if (this.errorCallback) {
+        this.errorCallback(err);
+      }
+    });
+
+    this.recognizeStream.on('finish', (res) => {});
+    console.log('[' + this.id + '] Stream Created');
+    this.waitingRestart = false;
+    return this.recognizeStream;
+  }
+
+  tryEndAudioStream() {
+    if (this.recognizeStream) {
+      this.recognizeStream.destroy();
+      this.recognizeStream.removeAllListeners('data');
+      this.recognizeStream = null;
+      console.log('[' + this.id + '] Stream Destroyed');
+    } else {
+      clearInterval(this.intervalRestart);
+    }
+  }
+  closeConversation() {
+    this.tryEndAudioStream();
+  }
+  async translate(text) {
+    let output = '';
+    try {
+      let [translations] = await this.translationClient.translate(
+        text,
+        this.outputLanguage
+      );
+      translations = Array.isArray(translations)
+        ? translations
+        : [translations];
+      translations.forEach((translation, i) => {
+        output += `${translation}`;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(output);
+    return output;
+  }
+
+  async playTTS(text, isSessionEnding = false) {
+    try {
+      this.sessionEnding = isSessionEnding;
+      const request = {
+        input: { text: text },
+        voice: { languageCode: this.outputLanguage, ssmlGender: 'NEUTRAL' },
+        audioConfig: {
+          audioEncoding: encoding,
+          sampleRateHertz: sampleRateHertz,
+        },
+      };
+      const [response] = await this.ttsClient.synthesizeSpeech(request);
+      if (this.audioChunkAvailableCallback) {
+        this.audioChunkAvailableCallback(response.audioContent);
+      }
+    } catch (err) {
+      if (this.errorCallback) {
+        this.errorCallback(err);
+      }
+    }
+  }
+}
+
+module.exports = StreamingClient;
